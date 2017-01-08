0e2e893853469587ed88486bdab8ffe04444ac52
nester <nesterd85@gmail.com>
2017-01-08 14:02:49 +0700

    описан базовый класс Bee

--- FunWithBees_page301/Bees/BeeHive/Base/Bee.cs
+++ FunWithBees_page301/Bees/BeeHive/Base/Bee.cs
@@ -8,5 +8,18 @@ namespace Bees.BeeHive.Base
 {
     public class Bee
     {
+        public const double honeyUnitsConsumedPerMg = .25;
+
+        public double WeightMg { get; private set; }
+
+        public Bee(double weightMg)
+        {
+            WeightMg = weightMg;
+        }
+
+        virtual public double HoneyConsumptionRate()
+        {
+            return WeightMg * honeyUnitsConsumedPerMg;
+        }
     }
 }
